26dca05e587e9bbb3a9622ff707b757b1f4368fd
damwilin <damian.wilinski@gmail.com>
2017-05-10 22:03:57 +0200

    Implemented DictionaryBST class

--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -4,23 +4,24 @@ import java.util.LinkedList;
 
 /**
  * A class that implements the Dictionary interface using a LinkedList
- *
  */
-public class DictionaryLL implements Dictionary 
-{
+public class DictionaryLL implements Dictionary {
 
-	private LinkedList<String> dict;
+    private LinkedList<String> dict;
 
     public DictionaryLL() {
         dict = new LinkedList<String>();
     }
 
 
-    /** Add this word to the dictionary.  Convert it to lowercase first
+    /**
+     * Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
+     *
      * @param word The word to add
-     * @return true if the word was added to the dictionary 
-     * (it wasn't already there). */
+     * @return true if the word was added to the dictionary
+     * (it wasn't already there).
+     */
     public boolean addWord(String word) {
         word = word.toLowerCase();
         if (dict.contains(word))
@@ -30,17 +31,19 @@ public class DictionaryLL implements Dictionary
     }
 
 
-    /** Return the number of words in the dictionary */
-    public int size()
-    {
+    /**
+     * Return the number of words in the dictionary
+     */
+    public int size() {
         return dict.size();
     }
 
-    /** Is this a word according to this dictionary? */
+    /**
+     * Is this a word according to this dictionary?
+     */
     public boolean isWord(String s) {
-        s = s.toLowerCase();
-        return dict.contains(s);
+        return dict.contains(s.toLowerCase());
     }
 
-    
+
 }
