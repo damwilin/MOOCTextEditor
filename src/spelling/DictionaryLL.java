e64fcd5eef9237facb37d92a83e33b072266ab70
damwilin <damian.wilinski@gmail.com>
2017-05-10 21:56:42 +0200

    Implemented DictionaryLL class

--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -10,8 +10,10 @@ public class DictionaryLL implements Dictionary
 {
 
 	private LinkedList<String> dict;
-	
-    // TODO: Add a constructor
+
+    public DictionaryLL() {
+        dict = new LinkedList<String>();
+    }
 
 
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -20,22 +22,24 @@ public class DictionaryLL implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+        word = word.toLowerCase();
+        if (dict.contains(word))
+            return false;
+        dict.add(word);
+        return true;
     }
 
 
     /** Return the number of words in the dictionary */
     public int size()
     {
-        // TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-        //TODO: Implement this method
-        return false;
+        s = s.toLowerCase();
+        return dict.contains(s);
     }
 
     
